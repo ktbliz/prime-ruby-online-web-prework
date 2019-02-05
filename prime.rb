18a3ea314693229e11073a46bbdcf7bd0f73bd00
Katie Blizzard <kblizzard11@gmail.com>
2019-02-05 02:29:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,23 @@
 # Add  code here!
 def prime?(x)
-  z = x - 1
-  array = []
-  for num in 2..z do 
-    remainder = (x % num).ceil
-    array.push(remainder)
-  end
-  if array.include?(0)
+  
+  if x < 0 
     puts false
-    return false 
-  else
-    puts true 
-    return true 
-  end 
+    return false
+  else 
+    z = x - 1
+    array = []
+    for num in 2..z do 
+      remainder = (x % num).ceil
+      array.push(remainder)
+    end
+    if array.include?(0)
+      puts false
+      return false 
+    else
+      puts true 
+      return true 
+    end 
 end
 
 
